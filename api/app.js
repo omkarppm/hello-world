cfdf474d385c577abdc85e7ce3ecc9f16b362199
Rohan <rohan@perituza.com>
2016-10-26 17:56:13 +0530

    changes done

--- api/app.js
+++ api/app.js
@@ -1,30 +1,4 @@
 function test(){
 	console.log(22112);
 }
-function test(){
-	console.log(2211cscdc2);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
-function test(){
-	console.log(22112);
-}
+
